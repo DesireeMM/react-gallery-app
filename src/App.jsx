f1f3d32e57ad2a53ad9a2c34f469f6f9569ae4d6
Desiree Morimoto <dmorimoto89@gmail.com>
2023-11-06 16:18:40 -0800

    Add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios';
 import {Route, Routes, Navigate} from 'react-router-dom';
 import apiKey from './config';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 // import Components
@@ -12,23 +10,38 @@ import Nav from './components/Nav';
 import PhotoList from './components/PhotoList'
 
 const App = () => {
+  const [query, setQuery] = useState("cats");
   const [photos, setPhotos] = useState([]);
 
+  useEffect(() => {
+    let activeFetch = true;
+
+    fetchData(query)
+
+    return () => {activeFetch = false}
+  }, [query])
+
   const fetchData = (query) => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}=${query}&safe_search=1&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then((response) => {
-        setPhotos(response.data)
-        console.log(photos);
+        setPhotos(response.data.photos.photo);
+      })
+      .catch((error) => {
+        console.log("An error occurred", error);
       })
   };
 
+  const handleQueryChange = (query) => {
+    setQuery(query);
+  }
+
   return (
     <>
-      <Search />
+      <Search changeQuery={handleQueryChange}/>
       <Nav />
       <Routes>
         <Route path="/" element={<Navigate to="/cats" />}/>
-        <Route path="/search/:query" element={<PhotoList />} />
+        <Route path="/search/:query" element={<PhotoList data={photos} title={query}/>} />
       </Routes>
     </>
   )
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+
+import Photo from './Photo';
 
 const PhotoList = (props) => {
+    const {paramQuery} = useParams();
+
     const searchResults = props.data;
     let photos;
     if (searchResults.length > 0) {
-        photos = results.map(photo => {
+        photos = searchResults.map(photo => {
             const photoUrl = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`
             return <Photo 
                 key={photo.id}
+                alt={photo.title}
                 src={photoUrl} />
         })
     } else {
-        photos = '<h3>No results</h3>'
+        photos = 'No results'
     }
 
     return (
         <div class="photo-container">
-        <h2>Results</h2>
+        <h2>{props.title} Results</h2>
         <ul>
             {photos}
         </ul>
